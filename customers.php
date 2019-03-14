734150c54b07d2e2d640a1ac3c9e17719f85fa0e
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2019-03-14 11:10:17 +0100

    Created main customer start and search features

--- customers.php
+++ customers.php
@@ -1,3 +1,22 @@
+<?php 
+$host = 'localhost';
+$db = 'classicmodels';
+$user = 'root';
+$pass = 'Carlphp2019';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;
+ dbname=$db;
+ charset=$charset";
+
+try {
+     $pdo = new PDO($dsn, $user, $pass);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(),(int)$e->getCode());
+}
+
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -8,9 +27,24 @@
     <title>Document</title>
 </head>
 <body>
-<?php
 
+<h1>Kunder</h1>
+<?php
+$stmt = $pdo->query("SELECT * FROM customers order by customerName limit 20");
+    
+    while ($row = $stmt->fetch())
+{
+    echo $row['customerNumber'] . " - " . $row['customerName'] . " - " .  $row['country'] .  "<br>";
+}    
 ?>
+<br>
+<a href="customersearch.php"><strong><i>Sök kunder</i></strong></a>
+
+<!-- <form action="customersearch.php" method="get">
+        <input type="text" name="customer">
+        <input type="submit" value="Search">   
+</form> -->
+
 </body>
 </html>
 
--- customersearch.php
+++ customersearch.php
@@ -1,3 +1,21 @@
+<?php
+$host = 'localhost';
+$db = 'classicmodels';
+$user = 'root';
+$pass = 'Carlphp2019';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;
+ dbname=$db;
+ charset=$charset";
+
+ try {
+    $pdo = new PDO($dsn, $user, $pass);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(),(int)$e->getCode());
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,8 +25,34 @@
     <title>Document</title>
 </head>
 <body>
+
+<h1>Sökresultat kunder</h1>
+<form action="customersearch.php" method="get">
+        <input type="text" name="customer">
+        <input type="submit" value="Search">   
+</form>
+<br>
 <?php
+if (isset($_GET['customer'])) {
+    $customerNumber = filter_input(INPUT_GET, 'customer', FILTER_SANITIZE_STRING);    
+
+    $stmt = $pdo->query("SELECT * FROM customers WHERE customerNumber = $customerNumber");
+
+    if ($row = $stmt->fetch()) {
+        print_r($row);
+        echo "<br>" . "<br>" .$row['customerNumber'] . " - " . $row['customerName'] . " - " .  $row['country'] .  "<br>";
+    } else {
+        echo 'Det finns ingen kund med det numret.';
+    }
+ } else {
+     echo 'Ingen kund vald.' . '<br>';
+}
+
+$customerNumber = '242';  
 
 ?>
+<br>
+<a href="customers.php"><strong><i>Kunder Startsida</i></strong></a>
+
 </body>
 </html>
