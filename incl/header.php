7c5aca7d9166597e46f51eb648531441544166e9
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2019-03-19 13:02:13 +0100

    Changed inclfolder name to incl and moved the incl files there

---
+++ incl/header.php
@@ -0,0 +1,30 @@
+<?php
+//include 'PDO.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible">
+    <!-- <link type="text/css" href="CssHeader.css" rel="stylesheet" /> -->
+
+    <title>DreamTeam Startpage</title>
+
+</head>
+
+<body>
+
+<div class="NavContainer">
+        <nav class="TopNav">
+            <a href="LoginPage.php" class="TopBtn" Id="LogInBtn">Logga In</a>
+            <a href="RegisterPage.php" class="TopBtn" Id="RegisterBtn">Registrera</a>
+            <a href="ProductPage.php" class="TopBtn" Id="ProductsBtn">Produkter</a>
+            <a href="KassaPage.php" class="TopBtn" Id="KassaBtn">Kassa</a>
+            <a href="OrderPage.php" class="TopBtn" Id="KassaBtn">Ordrar</a>
+        </nav>
+</div>
+
+<!-- </body>
+</html> -->
