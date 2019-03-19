022b6284affdd11b53e655b72860f2cafeab27c2
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2019-03-19 09:57:29 +0100

    Created custincl folder with customernavbar file and corresponding include instances for this navbar in the other files

---
+++ custincl/customernavbar.php
@@ -0,0 +1,6 @@
+<ul>
+    <li><a href="customers.php">Kunder startsida</a></li>
+    <li><a href="customersearch2.php">Sök kunder</a></li>
+    <li><a href="customeradmin.php">Administrera kunder</a></li>
+    <li><a href="newcustomer.php">Skapa ny kund</a></li>
+</ul>
