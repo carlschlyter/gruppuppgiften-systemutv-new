5c5675b3fbf648f1cbc7ceb9e10b2748bc5374c4
tomaa88 <47780292+tomaa88@users.noreply.github.com>
2019-03-19 13:01:18 +0100

    PDO-include

--- PDO.php
+++ PDO.php
@@ -8,36 +8,9 @@ $pass = '';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-
 try {
-     $pdo = new PDO($dsn, $user, $pass);
+    $pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_FUNC);
 } catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    throw new \PDOException($e->getMessage(),(int)$e->getCode());
 }
- /*class Database {
-     private static $pdo = NULL;
-     private static function connect() {
-        $host = 'localhost';
-        $db   = 'classicmodels';
-        $user = 'root';
-        $pass = '';
-        $charset = 'utf8mb4';
-         $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-         try {
-            static::$pdo = new PDO($dsn, $user, $pass);
-        } catch (\PDOException $e) {
-            throw new \PDOException($e->getMessage(), (int)$e->getCode());
-        }
-     }
-     public static function pdo() {
-         if(static::$pdo == NULL) {
-            static::connect();
-        }
-         return static::$pdo;
-     }
-
- }
- 
- 
- }*/
- ?>
